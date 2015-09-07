8d0f77ab222addb124b5e1e634ac7d58ea603eff
dmips <dominika.mips@amberbit.com>
2015-09-07 10:27:05 +0200

    16 - create user with sorcery + add role to user

--- webplatform/db/schema.rb
+++ webplatform/db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150907080605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                      null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "role",             limit: 2
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 end
