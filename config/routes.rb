d91e4cf11595d003cdeff413aa8b079a678855d9
pg20 <pramitigoel20@gmail.com>
2015-09-14 14:50:48 +0530

    loginpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Webplatform::Application.routes.draw do
+  #get 'user_sessions/new'
+
+  #get 'user_sessions/create'
+
+  #get 'user_sessions/destroy'
+
+  resources :user_sessions
   root 'home#index'
 
   resources :mentor_applications do
@@ -8,4 +15,7 @@ Webplatform::Application.routes.draw do
   resources :mentee_applications do
     resources :build, controller: 'mentee_applications/build'
   end
+  
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
