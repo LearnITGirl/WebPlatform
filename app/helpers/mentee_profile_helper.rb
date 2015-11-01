4b4b55527bedb2dbcdb844ca7d81d0f360a3379b
Katerina <ktrajchevska@gmail.com>
2015-11-01 21:24:23 +0100

    Create the layout and style for mentee profile pages

---
+++ app/helpers/mentee_profile_helper.rb
@@ -0,0 +1,2 @@
+module MenteeProfileHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Webplatform::Application.routes.draw do
   resource :user_sessions, only: [:create]
   root 'home#index'
   get  "first-edition" => "home#first_edition_projects"
-  get "sponsors" => "home#sponsors"
+  get  "sponsors" => "home#sponsors"
+  get  "mentee-profile" => "mentee_profile#index"
+  get  "mentee-dashboard" => "mentee_profile#dashboard"
 
   resources :mentor_applications do
     resources :build, controller: 'mentor_applications/build'
