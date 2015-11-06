9e7a69359a6a75ec823f6399c14e78580b47a355
dmips <dominika.mips@amberbit.com>
2015-11-06 22:22:29 +0100

    add styling to first edition projects, login and reset password pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Webplatform::Application.routes.draw do
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'logout' => 'user_sessions#destroy', :as => :logout
-  
+
   resource :user_sessions, only: [:create]
   root 'home#index'
-  get  "first-edition" => "home#first_edition_projects"
+  get  "first_edition" => "home#first_edition_projects"
   get "sponsors" => "home#sponsors"
 
   resources :mentor_applications do
@@ -15,7 +15,6 @@ Webplatform::Application.routes.draw do
   resources :mentee_applications do
     resources :build, controller: 'mentee_applications/build'
   end
-  
 
   resources :password_resets
 end
