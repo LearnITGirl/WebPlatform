21e808ede451bae6fb88d7d0aa9728551307a34e
pg20 <pramitigoel20@gmail.com>
2015-11-10 19:18:54 +0530

    check if user is organiser

--- app/controllers/organisers_controller.rb
+++ app/controllers/organisers_controller.rb
@@ -1,8 +1,13 @@
 class OrganisersController < ApplicationController
+   before_action :require_organiser
 
   def index
     @organisers = User.where(role: 1)
   end
 
-
-end
+  def require_organiser
+    unless current_user.role =="organiser"
+      redirect_to root_path, notice: "Login again as a organiser"
+    end
+  end
+end 
