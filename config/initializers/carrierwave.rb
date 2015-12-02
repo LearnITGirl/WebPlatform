d2d9792965447ffc72a1f093062a1efdaad01f6f
dmips <dominika.mips@amberbit.com>
2015-12-02 19:01:40 +0100

    require s3 credentials only on staging and production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,18 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
-    region: 'eu-west-1'
-  }
-
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
     config.root = "#{Rails.root}/public"
     config.asset_host = 'http://localhost:3000'
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
   else
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET'],
+      region: 'eu-west-1'
+    }
+
     config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-
-  config.fog_directory = ENV['S3_BUCKET_NAME']
-end 
+end
