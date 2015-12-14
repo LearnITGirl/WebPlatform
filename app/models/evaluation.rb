f591fd47e135f3fdab1d9a91d9a1fa888e2847b5
Rafal Maksymczuk <rafal.maksymczuk@amberbit.com>
2015-12-14 20:40:20 +0100

    Add evaluation uniqueness validation. Fix mentee total live score

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -3,6 +3,9 @@ class Evaluation < ActiveRecord::Base
   belongs_to :mentee_application
   belongs_to :user
 
+  validates_uniqueness_of :user_id, scope: :mentor_application_id
+  validates_uniqueness_of :user_id, scope: :mentee_application_id
+
   def application
     mentor_application || mentee_application
   end
