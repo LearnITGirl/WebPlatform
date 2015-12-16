a28b7897be2f0df9a5d77c7b25201adf628ed4a8
pg20 <pramitigoel20@gmail.com>
2015-12-16 14:16:13 +0530

    corrections

--- app/controllers/mentee_profiles_controller.rb
+++ app/controllers/mentee_profiles_controller.rb
@@ -5,7 +5,6 @@ class MenteeProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @mentor = User.find(@user.matched_id)
-
   end
   
   def dashboard
@@ -14,16 +13,26 @@ class MenteeProfilesController < ApplicationController
   
   def missing_mentor
     @user = current_user
-    if !@user.blank? 
-      @mentor = User.find(current_user.matched_id)
-      if @mentor.update_attribute(:is_missing, true)
-        MissingPersonsMailer.missing_mentor(@user).deliver_now
-      	redirect_to dashboard_mentee_profiles_path, notice: 'Organsiers Have been notified about the missing mentor'
-      else 
-      	redirect_to dashboard_mentee_profiles_path, notice: 'Couldnt send mail'
-      end
+    @mentor = User.find(current_user.matched_id)
+    if @mentor.update_attribute(:is_missing, true)
+      MissingPersonsMailer.missing_mentor(@user).deliver_now
+      redirect_to dashboard_mentee_profiles_path, notice: 'Organsiers Have been notified about the missing mentor'
     else 
-      redirect_to dashboard_mentee_profiles_path, notice: "Please login first"
+      redirect_to dashboard_mentee_profiles_path, notice: 'Couldnt send mail'
     end
   end
+  
+  private
+   def require_mentee
+    unless current_user && current_user.mentee?
+      redirect_to root_path, alert: "Login again as a Mentee!"
+    end
+   end
+
+   def require_mentee_or_organiser
+    unless current_user && (current_user.organizer? || current_user.mentee?)
+      redirect_to root_path, alert: "You're not authorized to access this page!"
+    end
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151215160837) do
 
   # These are extensions that must be enabled in order to support this database
