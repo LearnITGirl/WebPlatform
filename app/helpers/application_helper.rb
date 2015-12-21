9f7ded08437bf839094bbdd8968b3c0feb1f23b6
dmips <dominika.mips@amberbit.com>
2015-12-21 23:18:05 +0100

    add helper for home page depending on a user's role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
+
   def glyphicon *names
     content_tag :span, nil, class: names.map{|name| "glyphicon-#{name.to_s.gsub('_','-')}" } + ["glyphicon"]
+  end
+
+  def main_path
+    case current_user.try(:role)
+    when "organizer" then dashboard_organisers_path
+    when "mantor" then dashboard_mentor_profile_path
+    when "mentee" then mentee_dashboard_path
+    else
+      root_path
+    end
   end
 end
