863b24339c7fdd71aa31524827c7bba4e2fc9219
dmips <dominika.mips@amberbit.com>
2015-12-23 18:00:16 +0100

    update text in mentor and mentee application's emails

--- app/mailers/mentee_application_mailer.rb
+++ app/mailers/mentee_application_mailer.rb
@@ -1,6 +1,6 @@
 class MenteeApplicationMailer < ApplicationMailer
   def confirm_application(mentee)
     @mentee = mentee
-    mail(to: @mentee.email, subject: "Thank you for your application")
+    mail(to: @mentee.email, subject: "Learn IT Girl - Thank you for your application")
   end
 end
