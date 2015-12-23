43fa0b96fcb3e39a49bc77c964232abbb23eac55
dmips <dominika.mips@amberbit.com>
2015-12-23 21:13:41 +0100

    add missing links + fix github link error on mentee profile show page

--- app/controllers/mentee_profiles_controller.rb
+++ app/controllers/mentee_profiles_controller.rb
@@ -7,23 +7,22 @@ class MenteeProfilesController < ApplicationController
     @mentor = User.find(@user.matched_id)
     @project = Project.find_by(mentee_id: params[:id])
   end
-  
+
   def dashboard
     @mentor = User.find(current_user.matched_id)
     @project = Project.find_by(mentee_id: current_user.id)
 
   end
-  
+
   def missing_mentor
     @user = current_user
     @mentor = User.find(current_user.matched_id)
     if @mentor.update_attribute(:is_missing, true)
       MissingPersonsMailer.missing_mentor(@user).deliver_now
       redirect_to dashboard_mentee_profiles_path, notice: 'Organsiers Have been notified about the missing mentor'
-    else 
+    else
       redirect_to dashboard_mentee_profiles_path, notice: 'Couldnt send mail'
     end
   end
-  
 
 end
