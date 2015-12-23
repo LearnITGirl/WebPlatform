2dd92ba46a9da225d5098b9c35a6937f1df6dac7
pg20 <pramitigoel20@gmail.com>
2015-12-23 15:03:38 +0530

    added project table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221061334) do
+ActiveRecord::Schema.define(version: 20151215160837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,14 +94,6 @@ ActiveRecord::Schema.define(version: 20151221061334) do
     t.string   "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "projects_tables", force: :cascade do |t|
-    t.string  "project_title"
-    t.string  "language"
-    t.string  "description"
-    t.integer "mentor_id"
-    t.integer "mentee_id"
   end
 
   create_table "users", force: :cascade do |t|
