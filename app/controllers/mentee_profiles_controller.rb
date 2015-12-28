0ae66f20eea69040c12154e5bfb9b09f5b6cf6ce
pg20 <pramitigoel20@gmail.com>
2015-12-28 19:37:12 +0530

    removed mentor_id

--- app/controllers/mentee_profiles_controller.rb
+++ app/controllers/mentee_profiles_controller.rb
@@ -53,7 +53,7 @@ class MenteeProfilesController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:id, :first_name, :last_name, :country, :program_country, :timezone, :project_attributes => [:id, :title,:language,:description,:github_link, :mentor_id])
+    params.require(:user).permit(:id, :first_name, :last_name, :country, :program_country, :timezone, :project_attributes => [:id, :title,:language,:description,:github_link])
   end
   
 
