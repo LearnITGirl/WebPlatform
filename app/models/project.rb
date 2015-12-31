3842c4f50f2821234ad143d54861d9a2cafcdf75
pg20 <pramitigoel20@gmail.com>
2015-12-31 12:44:45 +0530

    smallchanges in controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,6 @@ class Project < ActiveRecord::Base
   belongs_to :mentor, class_name: 'User', foreign_key: "mentor_id"
   belongs_to :mentee, class_name: 'User', foreign_key: "mentee_id"
 
+  validates :mentee_id, presence: true, on: :update
+
 end
