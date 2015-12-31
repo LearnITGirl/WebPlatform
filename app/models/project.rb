0aeffa11bd086e71479d95f09b81c642a9ea7edc
pg20 <pramitigoel20@gmail.com>
2015-12-31 13:24:07 +0530

    Added validations in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,7 @@ class Project < ActiveRecord::Base
   belongs_to :mentee, class_name: 'User', foreign_key: "mentee_id"
 
   validates :mentee_id, presence: true, on: :update
+  validates :title, :language, :description, :github_link, presence: true, on: :update
+
 
 end
