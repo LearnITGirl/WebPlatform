b2d4fc84870d138942ef500ae5fee0f33360362b
dmips <dominika.mips@amberbit.com>
2016-01-11 22:36:10 +0100

    change saving other language for mentee application

--- app/helpers/mentee_applications_helper.rb
+++ app/helpers/mentee_applications_helper.rb
@@ -1,15 +1,2 @@
 module MenteeApplicationsHelper
-
-  def mentee_other_language_value
-    available_languages.keys.include?(@mentee.programming_language) ? nil : @mentee.programming_language
-  end
-
-  def other_value
-    available_languages.keys.include?(@mentee.programming_language) ? "other" : @mentee.programming_language
-  end
-
-  def other_known_language_value
-    other = @mentee.known_programming_languages.select{|a| !available_languages.keys.include?(a)}
-    other.present? ? other.reject{|a| a == "other"} : nil
-  end
 end
