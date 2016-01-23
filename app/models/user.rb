6469752d9f7b5d84a1d22667f3242159ff653d8b
dmips <dominika.mips@amberbit.com>
2016-01-23 23:04:27 +0100

    check if application has already assigned evaluator or not

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_one :project, foreign_key: "mentor_id"
   has_one :project, foreign_key: "mentee_id"
   accepts_nested_attributes_for :project
+
+  has_many :mentee_applications, foreign_key: "evaluator_id"
+  has_many :mentor_applications, foreign_key: "evaluator_id"
+
   mount_uploader :avatar, AvatarUploader
 
   enum role: {organizer: 1, mentee: 2, mentor: 3}
