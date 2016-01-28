7ee4d60bcaac2720bff01f683272a1bbbfb5298e
Alma Castillo Antolin <alma.castillo.antolin@gmail.com>
2016-01-28 23:29:04 +0100

    PHP learning material

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Webplatform::Application.routes.draw do
   get "mentee_dos_and_donts" => "home#mentee_dos_and_donts"
   get "faq" => "home#faq"
   get "timeline" => "home#timeline"
+  get "learning_material/php" => "learning_material#php"
 
   resources :mentee_profiles, only: [:show, :edit, :update] do
     collection do
