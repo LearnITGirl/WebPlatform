29b396319842124427b38254f78b2bfecc318468
dmips <dominika.mips@amberbit.com>
2016-02-01 19:01:41 +0100

    create edition's weeks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201082759) do
+ActiveRecord::Schema.define(version: 20160201174655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,5 +162,12 @@ ActiveRecord::Schema.define(version: 20160201082759) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
+
+  create_table "weeks", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.integer  "number"
+    t.integer  "edition_id"
+  end
 
 end
