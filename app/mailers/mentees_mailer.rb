839d7ee3f827d588701ad7e293f877ffceb61cfb
Anjali Sharma <sharma.anjali3110@gmail.com>
2016-02-05 10:29:34 +0530

    Remove mentee mailer

--- app/mailers/mentees_mailer.rb
+++
@@ -1,8 +0,0 @@
-class MenteesMailer < ApplicationMailer
-	def register(user)
-		@user = user
-		@url = edit_user_registration_url(@user.registration_token)
-		mail(to: user.email, 
-			subject: "[Learn IT, Girl] Congratulations! Welcome as a second edition mentee.")
-	end
-end
--- test/mailers/previews/mentees_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/mentees_mailer
-class MenteesMailerPreview < ActionMailer::Preview
-
-end
