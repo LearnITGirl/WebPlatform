8ca6e89bef546523469cdeb237869eb48eb7302e
dmips <dominika.mips@amberbit.com>
2016-02-08 18:31:31 +0100

    add link to matching page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,7 @@ module ApplicationHelper
     if current_user && current_user.organizer?
       links << (content_tag :li, link_to('Manage organizers', organisers_path), class: "no-image")
       links << (content_tag :li, link_to('Email templates', email_templates_path), class: "no-image")
+      links << (content_tag :li, link_to('Matched pairs', mentor_to_mentee_matchers_path), class: "no-image")
     end
     if current_user && current_user.mentee?
       links << (content_tag :li, link_to('Mentee do\'s and don\'ts', mentee_dos_and_donts_path), class: "no-image")
