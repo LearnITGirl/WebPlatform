df4e34ed6dc2edeab390f95d7939378822fbc72c
dmips <dominika.mips@amberbit.com>
2016-02-22 23:28:23 +0100

    remove tests

--- test/mailers/mentee_application_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MenteeApplicationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/mailers/previews/project_summary_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/project_summary_mailer
-class ProjectSummaryMailerPreview < ActionMailer::Preview
-
-end
