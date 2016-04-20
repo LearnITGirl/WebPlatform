f8f238aaec994dcc80ed5ca92c0cd6f592d4e4f7
Anjali Sharma <sharma.anjali3110@gmail.com>
2016-04-20 12:25:21 +0530

    Task update fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    if @task.update_attributes title: params[:task][:title]
+    if @task.update_attributes task_params
       head :ok
     else
       render json: {msg: @task.errors.full_messages.join(',')}, status: 422
@@ -48,10 +48,12 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:status, :week).tap do |task|
-      task[:status] = Task.statuses[task["status"]]
-      task[:finished_by] = current_user.id
-      task[:week] = find_week.number
+    params.require(:task).permit(:status, :title).tap do |task|
+      if task[:status].present?
+        task[:status] = Task.statuses[task["status"]]
+        task[:finished_by] = current_user.id if task[:status] == Task.statuses["finished"]
+        task[:week] = find_week.number
+      end
     end
   end
 
