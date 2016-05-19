f29aa9a708c0b04f22c92d4d794ef1ab8c490131
Anjali Sharma <sharma.anjali3110@gmail.com>
2016-05-19 09:37:53 +0530

    Add project view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,7 @@ class ProjectsController < ApplicationController
 
 	def show
 		@project = Project.find(params[:id])
+		@week = find_week
+		@tasks = @week.nil? ? @project.tasks.not_deleted : @project.week_tasks(@week.number).not_deleted
 	end
 end
