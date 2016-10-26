4d87e942c904b66fd2822d0d655f76594486cf7f
Rafal Maksymczuk <rafal.maksymczuk@amberbit.com>
2016-10-27 01:14:33 +0200

    Removing old mentee application wizard files

--- app/controllers/mentee_applications_controller.rb
+++ app/controllers/mentee_applications_controller.rb
@@ -1,8 +1,5 @@
 class MenteeApplicationsController < ApplicationController
-
   def new
     @mentee_application = MenteeApplication.new
-    #@mentee = MenteeApplication.create(build_step: :personal_information)
-    #redirect_to mentee_application_build_path(:personal_information, mentee_application_id: @mentee.id)
   end
 end
