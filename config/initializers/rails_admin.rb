ecff2bc32f8f3ae81d8fc52e5c8bfafd634a9278
Rafal Maksymczuk <rafal.maksymczuk@amberbit.com>
2017-06-24 23:50:27 +0200

    More rails admin configuration params

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,21 +1,32 @@
 RailsAdmin.config do |config|
+  config.main_app_name = ["Learn IT Girl"]
 
   config.authorize_with do
     redirect_to main_app.root_path unless current_user.try(:organizer?)
   end
 
-  #config.included_models = ["ProgrammingLanguage", "Badge", "Edition", "EmailTemplate", "User"]
+  config.included_models = ["ProgrammingLanguage", "Badge", "Edition", "User"]
+
+  config.model 'User' do
+    list do
+      field :first_name
+      field :last_name
+      field :email
+      field :country
+      field :role
+    end
+  end
 
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
     new
-    #export
-    #bulk_delete
     show
     edit
     delete
     show_in_app
+    #export
+    #bulk_delete
 
     ## With an audit adapter, you can add:
     # history_index
