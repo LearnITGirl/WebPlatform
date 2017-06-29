061812709b1253078c8346cafdec6d4b9f4b0fd6
dmips <dominika.mips@gmail.com>
2017-06-28 22:17:39 -0700

    modify migration

--- db/migrate/20160514060724_add_evaluation_attributes_to_project.rb
+++ db/migrate/20160514060724_add_evaluation_attributes_to_project.rb
@@ -1,6 +1,6 @@
 class AddEvaluationAttributesToProject < ActiveRecord::Migration
   def change
-  	add_column :projects, :mentor_evaluation, :integer
+  	change_column :projects, :mentor_evaluation, :integer
     add_column :projects, :mentee_feedback, :integer
     add_column :projects, :mentee_project_status, :integer
     add_column :projects, :github_repo_status, :integer
