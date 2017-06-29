8f51901f0a03addc7417a4f420ab974d31bdf027
dmips <dominika.mips@gmail.com>
2017-06-28 22:22:47 -0700

    make migration work

--- db/migrate/20160514060724_add_evaluation_attributes_to_project.rb
+++ db/migrate/20160514060724_add_evaluation_attributes_to_project.rb
@@ -1,9 +1,17 @@
 class AddEvaluationAttributesToProject < ActiveRecord::Migration
-  def change
-  	change_column :projects, :mentor_evaluation, :integer
+  def up
+    change_column :projects, :mentor_evaluation, 'integer USING CAST(mentor_evaluation AS integer)'
     add_column :projects, :mentee_feedback, :integer
     add_column :projects, :mentee_project_status, :integer
     add_column :projects, :github_repo_status, :integer
     add_column :projects, :midterm_evaluation_status, :integer, :default => 0
+  end
+
+  def down
+    change_column :projects, :mentor_evaluation, :string
+    remove_column :projects, :mentee_feedback, :integer
+    remove_column :projects, :mentee_project_status, :integer
+    remove_column :projects, :github_repo_status, :integer
+    remove_column :projects, :midterm_evaluation_status, :integer, :default => 0
   end
 end
