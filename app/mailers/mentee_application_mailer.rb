01e38e4d281536dd60ecc7e62a4d28e306a5be08
Rafal Maksymczuk <rafal.maksymczuk@amberbit.com>
2017-08-06 16:52:51 +0200

    add sender info to email 'from' field

--- app/mailers/mentee_application_mailer.rb
+++ app/mailers/mentee_application_mailer.rb
@@ -3,6 +3,6 @@ class MenteeApplicationMailer < ApplicationMailer
 
   def confirm_application(application)
     @application = application
-    mail(to: application.email, from: 'learnitgirl.office@gmail.com', subject: "Learn IT Girl - Thank you for your application")
+    mail(to: application.email, from: 'Learn IT Girl <learnitgirl.office@gmail.com>', subject: "Learn IT Girl - Thank you for your application")
   end
 end
