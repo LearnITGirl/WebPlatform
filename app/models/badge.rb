7d3b66421040c8c30f1a6be0f6da11431f2ea0e6
dmips <dominika.mips@gmail.com>
2017-08-17 22:40:43 -0700

    create seeds for all badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,10 @@
 class Badge < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  enum name: {
+    midterm_survey: 1, final_survey: 2, blogger: 3,
+    organized: 4, ambitious: 5, energetic: 6,
+    timekeeper: 7, novice: 8, perfectionist: 9,
+    coder: 10
+  }
 end
