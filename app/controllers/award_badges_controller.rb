db3958f7174cabd981ab778c930e88b6b02b0024
pramiti <pramitigoel20@gmail.com>
2017-08-27 02:12:58 +0530

    sending params using button

--- app/controllers/award_badges_controller.rb
+++ app/controllers/award_badges_controller.rb
@@ -1,25 +1,20 @@
 class AwardBadgesController < ApplicationController
 
   def index
-	@projects = Project.where.not(mentee_id: Badge.where(name: [11]).includes(:users).map{|b| b.users.pluck(:id)}.flatten)
+    @projects = Project.where.not(mentee_id: Badge.where(name: [11]).includes(:users).map{|b| b.users.pluck(:id)}.flatten)
   end
 
   def create 
-	assign_visionary_badge
-	#redirect_to award_badges_path
-	render "index"
+    @curr_project = Project.find(params[:proj_id])
+    assign_visionary_badge
+    redirect_to award_badges_path
   end
 
-   def assign_visionary_badge
-      unless @project.mentee.badges.visionary.any?
-        @project.mentee.badges << Badge.visionary
-      end
-   end
 
-   def assign_organized_badge
-      unless @project.mentee.badges.organized.any?
-        @project.mentee.badges << Badge.organized
+  def assign_visionary_badge
+      unless @curr_project.mentee.badges.visionary.any?
+        @curr_project.mentee.badges << Badge.visionary
       end
-   end
+  end
 
 end
