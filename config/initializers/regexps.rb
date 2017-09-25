be95e776407ceba1f82b1c655411c812a8cf30ad
dmips <dominika.mips@gmail.com>
2017-09-25 08:38:05 -0700

    accept only https github links

--- config/initializers/regexps.rb
+++ config/initializers/regexps.rb
@@ -1,4 +1,5 @@
 REGEXP_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9_]+\.)+[a-z]{2,})\Z/i
 REGEXP_LETTER = /\A\D+\Z/
 REGEXP_NUMBER = /\A[\d-]+\Z/
-GITHUB_REGEXP = /\A(.*)(\/|:)(.*)\/(.*).git\Z/
+GITHUB_SSH_REGEXP = /\A(.*)(\/|:)(.*)\/(.*).git\Z/
+GITHUB_REGEXP = /\A(https:\/\/|http:\/\/)?(www.)?github.com\/[0-9a-zA-Z\-_]+\/[0-9a-zA-Z\-_]+\/*\z/
