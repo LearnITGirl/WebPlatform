6c7d58200fd4cab54f7e3185ef983ce2cdf8eeac
dmips <dominika.mips@gmail.com>
2017-09-24 18:30:05 -0700

    allow only mentee/mentor with assigned partner to access the platform

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:session][:email].downcase, params[:session][:password])
-      redirect_to user_dashboard_path
+      check_partner
     else
       flash.now[:alert] = "Email and password don't match"
       render action: 'new'
