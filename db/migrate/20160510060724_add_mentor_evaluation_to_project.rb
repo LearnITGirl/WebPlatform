999341434d18241592fa7b397f8143dc15ce8a03
dmips <dominika.mips@gmail.com>
2017-10-15 19:15:14 -0700

    fix migration

--- db/migrate/20160510060724_add_mentor_evaluation_to_project.rb
+++ db/migrate/20160510060724_add_mentor_evaluation_to_project.rb
@@ -1,5 +1,5 @@
 class AddMentorEvaluationToProject < ActiveRecord::Migration
   def up
-    add_column :projects, :mentor_evaluation, :integer
+    add_column :projects, :mentor_evaluation, :integer unless Project.column_names.include?("mentor_evaluation")
   end
 end
