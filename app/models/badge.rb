f0596cee112b76791a9910f8d17b07ca9e51d18c
dmips <dominika.mips@gmail.com>
2017-10-21 20:32:23 -0700

    remove perfectionist badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,10 +2,12 @@ class Badge < ActiveRecord::Base
   has_many :assigned_badges
   has_many :users, through: :assigned_badges
 
+  #TODO: correct number for next edition
+
   enum name: {
     midterm_survey: 1, final_survey: 2,
     organized: 3, ambitious: 4, energetic: 5,
-    timekeeper: 6, novice: 7, perfectionist: 8,
+    timekeeper: 6, novice: 7,
     coder: 9, visionary: 10
   }
 end
