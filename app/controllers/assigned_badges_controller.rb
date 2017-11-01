95e5cc49c3a34570ced0a738ea53956b1806298d
Anjali Sharma <anjali.sharma@zoomcar.com>
2017-11-02 01:26:09 +0530

    Update description for timeline badges

--- app/controllers/assigned_badges_controller.rb
+++ app/controllers/assigned_badges_controller.rb
@@ -2,7 +2,7 @@ class AssignedBadgesController < ApplicationController
   before_action :require_mentee
 
   def update
-    badge = current_user.assigned_badges.find_by(badge_id: params[:badge_id])
+    badge = current_user.assigned_badges.where(badge_id: params[:badge_id]).last
     badge.update(displayed: true)
     head :ok
   end
