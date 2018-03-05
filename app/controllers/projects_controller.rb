d1fc7d5b79fe6f980f11b6510465063ca9a667c4
dmips <dominika.mips@gmail.com>
2018-03-05 08:27:01 -0800

    fix tasks display for evaluation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,9 @@
 class ProjectsController < ApplicationController
 
-	def show
-		@project = Project.find(params[:id])
-		@week = find_week
-		@tasks = @week.nil? ? @project.tasks.not_deleted : @project.week_tasks(@week.number).not_deleted
-	end
+  def show
+    @project = Project.find(params[:id])
+    @week = find_week
+    @tasks = @week.nil? ? @project.tasks.not_deleted : @project.week_tasks(@week.number).not_deleted
+    @unfinished_tasks = @week.nil? ? @tasks.unfinished(current_user).order(:week) : @project.tasks.for_weeks(@week.number).unfinished(current_user).not_deleted.order(:week)
+  end
 end
