4dd7310eb86e5c0cfbe4ad0ea1607aadecb3215b
lucasandrad <lucasandrad@yandex.com>
2018-03-15 11:07:50 -0300

    Change files with wrong names

--- spec/controllers/assigned_badges_controller_spec.1.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AssignedBadgesController, type: :controller do
-	describe 'Assigned Badges Controller' do
-
-	end
-end
--- spec/controllers/assigned_badges_controller_spec.2.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AssignedBadgesController, type: :controller do
-	describe 'Assigned Badged Controller' do
-
-	end
-end
