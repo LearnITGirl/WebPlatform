b4c2cf316474420995e2ace1899f92b20b834d64
dmips <dominika.mips@gmail.com>
2019-03-10 11:08:03 -0700

    init week/edition in project helper for roadmap

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,11 @@
 module ProjectsHelper
 
   def setup_project(project)
-    12.times { project.roadmap_entries.build } unless project.roadmap_entries.any?
+    unless project.roadmap_entries.any?
+      current_edition.weeks.order(:number).each do |w|
+        project.roadmap_entries.build(week_id: w.id, edition_id: current_edition.id)
+      end
+    end
     project
   end
 end
