1a072cc13a3c131c312263fc2c17f8fea7015b4a
Pramiti Goel <pramitigoel@Pramitis-MacBook-Pro.local>
2022-03-26 23:43:28 +0800

    add migration version

--- db/migrate/20190507012334_add_program_feedback_to_midterm_evaluations.rb
+++ db/migrate/20190507012334_add_program_feedback_to_midterm_evaluations.rb
@@ -1,4 +1,4 @@
-class AddProgramFeedbackToMidtermEvaluations < ActiveRecord::Migration
+class AddProgramFeedbackToMidtermEvaluations < ActiveRecord::Migration[6.1]
   def change
   	add_column :mentee_midterm_evaluations, :program_feedback, :text, :after => :mentor_impression
   	add_column :mentor_midterm_evaluations, :program_feedback, :text, :after => :mentee_impression
